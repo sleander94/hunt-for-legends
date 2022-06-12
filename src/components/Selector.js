6a29a7c542545b06b9aaebb4d84d1dfe8eb0e2ac
Stephen Leander <stephen.leander1@gmail.com>
2022-06-12 18:34:27 +0200

    Add function to check coords for char on click

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -2,19 +2,33 @@ import React from 'react';
 import styles from './Selector.module.css';
 
 const Selector = ({ chars, x, y }) => {
+  const checkCoords = (char) => {
+    char = chars[char];
+    if (
+      x < char['xmax'] &&
+      x > char['xmin'] &&
+      y < char['ymax'] &&
+      y > char['ymin']
+    ) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  };
+
   return (
     <div
       id="selector"
       className={styles.selector}
       style={{ top: y * 100 + '%', left: x * 100 + '%' }}
     >
-      <div className="char" onClick={() => console.log(chars['char1']['name'])}>
+      <div className="char" onClick={() => checkCoords('char1')}>
         {chars['char1']['name']}
       </div>
-      <div className="char" onClick={() => console.log(chars['char2']['name'])}>
+      <div className="char" onClick={() => checkCoords('char2')}>
         {chars['char2']['name']}
       </div>
-      <div className="char" onClick={() => console.log(chars['char3']['name'])}>
+      <div className="char" onClick={() => checkCoords('char3')}>
         {chars['char3']['name']}
       </div>
     </div>

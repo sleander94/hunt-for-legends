5763b46453e5c9baf03832688a0f1aead615cc3f
Stephen Leander <stephen.leander1@gmail.com>
2022-06-12 12:24:53 +0200

    Initial GameGrid commit

---
+++ src/components/GameGrid.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './GameGrid.module.css';
+
+const GameGrid = () => {
+  return <div id="gameGrid" className={styles.gameGrid}></div>;
+};
+
+export default GameGrid;

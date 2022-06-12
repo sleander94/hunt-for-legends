ffbb74f3a73fde7b2a94cd9f90243c55f3ae1127
Stephen Leander <stephen.leander1@gmail.com>
2022-06-12 14:23:25 +0200

    Render Selector at mouse location onClick

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GameImage from './GameImage';
 import GameGrid from './GameGrid';
+import Selector from './Selector';
 import styles from './GameBoard.module.css';
 
 const GameBoard = () => {
+  const [selector, setSelector] = useState(false);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const chars = [
+    { name: 'Pengu' },
+    { name: 'Molediver' },
+    { name: 'Hauntling' },
+  ];
+
+  const displaySelector = (e) => {
+    let canvas = document.getElementById('gameGrid').getBoundingClientRect();
+    let x = e.clientX - canvas.left;
+    let y = e.clientY - canvas.top;
+    setX((x / canvas.width).toFixed(2));
+    setY((y / canvas.height).toFixed(2));
+    setSelector(!selector);
+  };
+
   return (
     <div id="gameBoard" className={styles.gameBoard}>
       <GameImage image="teamfight-tactics-wallpaper-1.png"></GameImage>
-      <GameGrid></GameGrid>
+      <GameGrid displaySelector={displaySelector}></GameGrid>
+      {selector && <Selector chars={chars} x={x} y={y}></Selector>}
     </div>
   );
 };

921d0971b3bee7cdc8ed7fca4b29b9039185dd0e
Stephen Leander <stephen.leander1@gmail.com>
2022-06-12 14:21:09 +0200

    Pass displaySelector prop from GameBoard

--- src/components/GameGrid.js
+++ src/components/GameGrid.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import styles from './GameGrid.module.css';
 
-const GameGrid = () => {
-  return <div id="gameGrid" className={styles.gameGrid}></div>;
+const GameGrid = ({ displaySelector }) => {
+  return (
+    <div
+      id="gameGrid"
+      className={styles.gameGrid}
+      onClick={(e) => displaySelector(e)}
+    ></div>
+  );
 };
 
 export default GameGrid;

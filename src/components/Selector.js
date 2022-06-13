f0dff3f85fcad189308683458cf04a38ef3dec75
Stephen Leander <stephen.leander1@gmail.com>
2022-06-13 09:08:48 +0200

    Refactor checkCoords input

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,34 +1,20 @@
 import React from 'react';
 import styles from './Selector.module.css';
 
-const Selector = ({ chars, x, y }) => {
-  const checkCoords = (char) => {
-    char = chars[char];
-    if (
-      x < char['xmax'] &&
-      x > char['xmin'] &&
-      y < char['ymax'] &&
-      y > char['ymin']
-    ) {
-      console.log(true);
-    } else {
-      console.log(false);
-    }
-  };
-
+const Selector = ({ chars, x, y, checkCoords }) => {
   return (
     <div
       id="selector"
       className={styles.selector}
       style={{ top: y * 100 + '%', left: x * 100 + '%' }}
     >
-      <div className="char" onClick={() => checkCoords('char1')}>
+      <div className="char" onClick={() => checkCoords(1)}>
         {chars['char1']['name']}
       </div>
-      <div className="char" onClick={() => checkCoords('char2')}>
+      <div className="char" onClick={() => checkCoords(2)}>
         {chars['char2']['name']}
       </div>
-      <div className="char" onClick={() => checkCoords('char3')}>
+      <div className="char" onClick={() => checkCoords(3)}>
         {chars['char3']['name']}
       </div>
     </div>

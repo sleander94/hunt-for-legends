61e796be6cc994d0037aed1c3f1ae35710e3067a
Stephen Leander <stephen.leander1@gmail.com>
2022-06-13 16:21:16 +0200

    Add images

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import styles from './Selector.module.css';
 
-const Selector = ({ chars, x, y, checkCoords }) => {
+const Selector = ({ chars, x, y, checkCoords, charURLs }) => {
   return (
     <div
       id="selector"
       className={styles.selector}
       style={{ top: y * 100 + '%', left: x * 100 + '%' }}
     >
-      <div className="char" onClick={() => checkCoords(1)}>
+      <div className={styles.char} onClick={() => checkCoords(1)}>
+        <img src={charURLs[0]}></img>
         {chars['char1']['name']}
       </div>
-      <div className="char" onClick={() => checkCoords(2)}>
+      <div className={styles.char} onClick={() => checkCoords(2)}>
+        <img src={charURLs[1]}></img>
         {chars['char2']['name']}
       </div>
-      <div className="char" onClick={() => checkCoords(3)}>
+      <div className={styles.char} onClick={() => checkCoords(3)}>
+        <img src={charURLs[2]}></img>
         {chars['char3']['name']}
       </div>
     </div>

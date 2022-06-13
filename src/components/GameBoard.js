2b33422e05de021702d42f12e1dbfee805b84c7f
Stephen Leander <stephen.leander1@gmail.com>
2022-06-13 09:10:55 +0200

    Add checkCoords funct for finding chars & wrap functs with if to prevent rerender loop

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -10,7 +10,8 @@ const GameBoard = ({ image }) => {
   const [selector, setSelector] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [chars, setChars] = useState({});
+  const [chars, setChars] = useState(null);
+  const [found, setFound] = useState([]);
 
   const getCharInfo = async () => {
     const response = await getDoc(doc(db, `game-images/${image}`));
@@ -18,23 +19,53 @@ const GameBoard = ({ image }) => {
   };
 
   useEffect(() => {
-    getCharInfo();
+    if (!chars) {
+      getCharInfo();
+      console.log('Getting chars');
+    }
   }, [chars]);
 
+  const checkCoords = (num) => {
+    const char = chars['char' + num];
+    if (!found.includes(num)) {
+      if (
+        x < char['xmax'] &&
+        x > char['xmin'] &&
+        y < char['ymax'] &&
+        y > char['ymin']
+      ) {
+        console.log(true);
+        setFound([...found, num]);
+        console.log(found);
+      } else {
+        console.log(false);
+      }
+    }
+  };
+
   const displaySelector = (e) => {
-    let canvas = document.getElementById('gameGrid').getBoundingClientRect();
-    let x = (e.clientX - canvas.left) / canvas.width;
-    let y = (e.clientY - canvas.top) / canvas.height;
-    setX(x.toFixed(2));
-    setY(y.toFixed(2));
-    setSelector(!selector);
+    if (chars) {
+      let canvas = document.getElementById('gameGrid').getBoundingClientRect();
+      let x = (e.clientX - canvas.left) / canvas.width;
+      let y = (e.clientY - canvas.top) / canvas.height;
+      setX(x.toFixed(2));
+      setY(y.toFixed(2));
+      setSelector(!selector);
+    }
   };
 
   return (
     <div id="gameBoard" className={styles.gameBoard}>
       <GameImage image={image + '.png'}></GameImage>
       <GameGrid displaySelector={displaySelector}></GameGrid>
-      {selector && <Selector chars={chars} x={x} y={y}></Selector>}
+      {selector && (
+        <Selector
+          chars={chars}
+          x={x}
+          y={y}
+          checkCoords={checkCoords}
+        ></Selector>
+      )}
     </div>
   );
 };

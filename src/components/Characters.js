a99e3abb4467e91e8b30b42fc1c94064565a2a39
Stephen Leander <stephen.leander1@gmail.com>
2022-06-15 14:01:31 +0200

    Add alt text to images

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -5,15 +5,15 @@ const Characters = ({ chars, charURLs }) => {
   return (
     <div className={styles.characterContainer}>
       <div className={styles.char} name={chars['char1']['name']}>
-        <img src={charURLs[0]} className={styles.image} />
+        <img src={charURLs[0]} alt="character icon" className={styles.image} />
         <div className={styles.charName}>{chars['char1']['name']}</div>
       </div>
       <div className={styles.char} name={chars['char2']['name']}>
-        <img src={charURLs[1]} className={styles.image} />
+        <img src={charURLs[1]} alt="character icon" className={styles.image} />
         <div className={styles.charName}>{chars['char2']['name']}</div>
       </div>
       <div className={styles.char} name={chars['char3']['name']}>
-        <img src={charURLs[2]} className={styles.image} />
+        <img src={charURLs[2]} alt="character icon" className={styles.image} />
         <div className={styles.charName}>{chars['char3']['name']}</div>
       </div>
     </div>

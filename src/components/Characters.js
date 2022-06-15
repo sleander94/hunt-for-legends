2ea14b8f35adb9f89c90b7144f599088b2951bfc
Stephen Leander <stephen.leander1@gmail.com>
2022-06-15 14:30:16 +0200

    Add alt text to images

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -5,15 +5,27 @@ const Characters = ({ chars, charURLs }) => {
   return (
     <div className={styles.characterContainer}>
       <div className={styles.char} name={chars['char1']['name']}>
-        <img src={charURLs[0]} alt="character icon" className={styles.image} />
+        <img
+          src={charURLs[0]}
+          alt="character 1 icon"
+          className={styles.image}
+        />
         <div className={styles.charName}>{chars['char1']['name']}</div>
       </div>
       <div className={styles.char} name={chars['char2']['name']}>
-        <img src={charURLs[1]} alt="character icon" className={styles.image} />
+        <img
+          src={charURLs[1]}
+          alt="character 2 icon"
+          className={styles.image}
+        />
         <div className={styles.charName}>{chars['char2']['name']}</div>
       </div>
       <div className={styles.char} name={chars['char3']['name']}>
-        <img src={charURLs[2]} alt="character icon" className={styles.image} />
+        <img
+          src={charURLs[2]}
+          alt="character 3 icon"
+          className={styles.image}
+        />
         <div className={styles.charName}>{chars['char3']['name']}</div>
       </div>
     </div>
--- src/components/Selector.js
+++ src/components/Selector.js
@@ -9,15 +9,15 @@ const Selector = ({ chars, x, y, checkCoords, charURLs }) => {
       style={{ top: y * 100 + '%', left: x * 100 + '%' }}
     >
       <div className={styles.char} onClick={() => checkCoords(1)}>
-        <img src={charURLs[0]}></img>
+        <img src={charURLs[0]} alt="character 1 icon"></img>
         {chars['char1']['name']}
       </div>
       <div className={styles.char} onClick={() => checkCoords(2)}>
-        <img src={charURLs[1]}></img>
+        <img src={charURLs[1]} alt="character 2 icon"></img>
         {chars['char2']['name']}
       </div>
       <div className={styles.char} onClick={() => checkCoords(3)}>
-        <img src={charURLs[2]}></img>
+        <img src={charURLs[2]} alt="character 3 icon"></img>
         {chars['char3']['name']}
       </div>
     </div>

38b32fc2beb1c5df834e7a3acc4f384c49917710
Stephen Leander <stephen.leander1@gmail.com>
2022-06-17 11:49:51 +0200

    Wrap in container div for positioning

--- src/components/ScoreUploader.js
+++ src/components/ScoreUploader.js
@@ -14,24 +14,26 @@ const ScoreUploader = ({ timer, name }) => {
   };
 
   return (
-    <div id="scoreUploader" className={styles.scoreUploader}>
-      <div className={styles.time}>
-        You finished the hunt in {timer} seconds!
+    <div className={styles.uploaderContainer}>
+      <div id="scoreUploader" className={styles.scoreUploader}>
+        <div className={styles.time}>
+          You finished the hunt in {timer} seconds!
+        </div>
+        <div className={styles.instructions}>
+          Enter your name to add your score to the leaderboard.
+        </div>
+        <input id="playerName" autoFocus></input>
+        <Link to="/home" className={styles.cancel}>
+          Cancel
+        </Link>
+        <Link
+          to={`/leaderboards/${name}`}
+          onClick={() => uploadScore()}
+          className={styles.submit}
+        >
+          Submit
+        </Link>
       </div>
-      <div className={styles.instructions}>
-        Enter your name to add your score to the leaderboard.
-      </div>
-      <input id="playerName" autoFocus></input>
-      <Link to="/home" className={styles.cancel}>
-        Cancel
-      </Link>
-      <Link
-        to={`/leaderboards/${name}`}
-        onClick={() => uploadScore()}
-        className={styles.submit}
-      >
-        Submit
-      </Link>
     </div>
   );
 };
